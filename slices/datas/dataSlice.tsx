9a10fe44b5b73897a7ced1615064ebbb57a8385f
ORLVNIMUSIC <orlvnimusic@gmail.com>
2022-10-28 00:54:52 +0300

    ts fix

--- slices/datas/dataSlice.tsx
+++ slices/datas/dataSlice.tsx
@@ -1,55 +1,55 @@
-import { createSlice } from '@reduxjs/toolkit'
-import type { PayloadAction } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
 
 export interface CounterState {
-  value: object[] ,
-  readOnlyValue: object[]
+  value: { id: number }[];
+  readOnlyValue: { id: number }[];
 }
 
 const initialState: CounterState = {
-  value:[],
-  readOnlyValue:[]
-}
-
-
+  value: [],
+  readOnlyValue: [],
+};
 
 export const dataSlice = createSlice({
   name: 'datas',
   initialState,
   reducers: {
-    uploadAllFirst:(state, action: PayloadAction<object[]>)=>{
-      state.readOnlyValue = action.payload
-
+    uploadAllFirst: (state, action: PayloadAction<{ id: number }[]>) => {
+      state.readOnlyValue = action.payload;
     },
-    uploadAll:(state)=>{    
-        state.value = state.readOnlyValue
+    uploadAll: (state) => {
+      state.value = state.readOnlyValue;
     },
-    uploadAllRemove:(state)=>{    
-      state.value =[]
-  },
+    uploadAllRemove: (state) => {
+      state.value = [];
+    },
     uploadAdd: (state, action: PayloadAction<string>) => {
-          let clone: object[] = state.value.slice()
-          state.readOnlyValue.forEach((x: any) => {
-            let i: string = action.payload
-            if (x.userId === Number(i)) {
-              clone.push(x)
-            }
-          })
-          state.value = clone
-         
-      
-     
+      let clone: { id: number }[] = state.value.slice();
+      state.readOnlyValue.forEach((x: any) => {
+        let i: string = action.payload;
+        if (x.userId === Number(i)) {
+          clone.push(x);
+        }
+      });
+      state.value = clone;
     },
-    uploadRemove:(state, action: PayloadAction<string>)=>{
-      let clone: object[] = state.value.slice()
-      let i: string = action.payload
-      clone = clone.filter((x:any)=>x.userId !== Number(i))
-      state.value = clone
-    }
+    uploadRemove: (state, action: PayloadAction<string>) => {
+      let clone: { id: number }[] = state.value.slice();
+      let i: string = action.payload;
+      clone = clone.filter((x: any) => x.userId !== Number(i));
+      state.value = clone;
+    },
   },
-})
+});
 
 // Action creators are generated for each case reducer function
-export const { uploadAdd, uploadAll, uploadRemove ,uploadAllFirst, uploadAllRemove} = dataSlice.actions
+export const {
+  uploadAdd,
+  uploadAll,
+  uploadRemove,
+  uploadAllFirst,
+  uploadAllRemove,
+} = dataSlice.actions;
 
-export default dataSlice.reducer
+export default dataSlice.reducer;
